b30a091867051db0246c75b8cb177dd5acdd7ada
Jason Chambers (jaschamb) <jaschamb@cisco.com>
2021-04-03 18:28:57 -0600

    Added MAC address parser

--- NetworkElementParsing.hs
+++ NetworkElementParsing.hs
@@ -13,6 +13,10 @@
 
 -- ipaddress ::= octet.octet.octet.octet
 -- MAC Address is a 12-digit hexadecimal number (6-Byte binary number), which is mostly represented by Colon-Hexadecimal notation
+-- macaddress := oui<macdelimiter><hexdigit><hexdigit><macdelimiter><hexdigit><hexdigit><macdelimiter><hexdigit><hexdigit>
+-- oui := <hexdigit><hexdigit><macdelimiter><hexdigit><hexdigit><macdelimiter><hexdigit><hexdigit>
+-- macdelimiter := - | :
+-- 8c:85:90:07:25:a3
 
 import Parsing
 import Data.Char
@@ -20,8 +24,14 @@ import Data.Char
 isOctet :: Int -> Bool
 isOctet n = n <= 255
 
+isMacDelimiter :: Char -> Bool
+isMacDelimiter c = c == ':' || c == '-'
+
 hexdigit :: Parser Char
 hexdigit = sat isHexDigit
+
+macdelimiter :: Parser Char
+macdelimiter = sat isMacDelimiter
 
 octet :: Parser String
 octet = do xs <- some digit
@@ -39,3 +49,24 @@ ipaddress = do a <- octet
                char '.'
                d <- octet
                return (a ++ "." ++ b ++ "." ++ c ++ "." ++ d)
+
+macaddress :: Parser String
+macaddress = do nib1 <- hexdigit
+                nib2 <- hexdigit
+                delim <- macdelimiter
+                nib3 <- hexdigit
+                nib4 <- hexdigit
+                delim <- macdelimiter
+                nib5 <- hexdigit
+                nib6 <- hexdigit
+                delim <- macdelimiter
+                nib7 <- hexdigit
+                nib8 <- hexdigit
+                delim <- macdelimiter
+                nib9 <- hexdigit
+                nib10 <- hexdigit
+                delim <- macdelimiter
+                nib11 <- hexdigit
+                nib12 <- hexdigit
+                return ([nib1] ++ [nib2] ++ [delim] ++ [nib3] ++ [nib4] ++ [delim] ++ [nib5] ++ [nib6] ++ [delim] ++ 
+                        [nib7] ++ [nib8] ++ [delim] ++ [nib9] ++ [nib10] ++ [delim] ++ [nib11] ++ [nib12])
